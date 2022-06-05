e10ec078644a2aea8113613f07206fc868d75852
Kevin <kevin@fritsch.vip>
2022-06-05 09:33:59 +0200

    added start functionality

--- team-paper/src/main/java/net/gigaclub/team/Main.java
+++ team-paper/src/main/java/net/gigaclub/team/Main.java
@@ -1,12 +1,41 @@
 package net.gigaclub.team;
 
+import net.gigaclub.teamapi.Team;
+import net.gigaclub.translation.Translation;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class Main extends JavaPlugin {
+
+    private static Main plugin;
+    private static Translation translation;
+    private static Team team;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        setPlugin(this);
+
+        File file = new File("plugins//" + "Odoo", "config.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
+        setTranslation(new Translation(
+                config.getString("Odoo.Host"),
+                config.getString("Odoo.Database"),
+                config.getString("Odoo.Username"),
+                config.getString("Odoo.Password")
+        ));
+        translation.setCategory("team");
+
+        setTeam(new Team(
+                config.getString("Odoo.Host"),
+                config.getString("Odoo.Database"),
+                config.getString("Odoo.Username"),
+                config.getString("Odoo.Password")
+        ));
 
     }
 
@@ -14,4 +43,29 @@ public final class Main extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static Main getPlugin() {
+        return plugin;
+    }
+
+    public static void setPlugin(Main plugin) {
+        Main.plugin = plugin;
+    }
+
+    public static Translation getTranslation() {
+        return Main.translation;
+    }
+
+    public static void setTranslation(Translation translation) {
+        Main.translation = translation;
+    }
+
+    public static Team getTeam() {
+        return Main.team;
+    }
+
+    public static void setTeam(Team team) {
+        Main.team = team;
+    }
+
 }
